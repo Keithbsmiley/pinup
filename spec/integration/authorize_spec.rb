d4737ff8fb931c202df06960a8f47ac94e76b248
Keith Smiley <keithbsmiley@gmail.com>
2013-04-25 19:23:35 -0400

    Spec for authorizing with netrc

--- spec/integration/authorize_spec.rb
+++ spec/integration/authorize_spec.rb
@@ -1,5 +1,22 @@
 require_relative '../spec_helper'
 
 describe Pinup::Authorize do
+  before do
+    @netrc_path = File.expand_path('~/foobar')
+  end
 
+  describe 'authorize_netrc' do
+    describe 'an empty netrc' do
+      it 'should return nil' do
+        expect(Pinup::Authorize.authorize_netrc({ path: @netrc_path })).to be_nil
+      end
+    end
+
+    describe 'a valid netrc' do
+      it 'should return true' do
+        # This will only pass if your default netrc has a valid username and password (token)
+        expect(Pinup::Authorize.authorize_netrc).to be_true
+      end
+    end
+  end
 end
