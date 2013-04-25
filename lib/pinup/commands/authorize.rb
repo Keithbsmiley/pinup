0bf334a452450ba3b31d6f94321d18be50e35901
Keith Smiley <keithbsmiley@gmail.com>
2013-04-25 10:34:18 -0400

    Working on authorize command

--- lib/pinup/commands/authorize.rb
+++ lib/pinup/commands/authorize.rb
@@ -1,3 +1,5 @@
+require 'netrc'
+
 module Pinup
   class Authorize
     def self.authorize(options)
@@ -5,13 +7,34 @@ module Pinup
       puts 'authorizerrr'
 
       if options[:netrc]
-        authorize_netrc
+        authorize_netrc(options)
       else
         authorize_credentials
       end
     end
 
-    def self.authorize_netrc
+    def self.authorize_netrc(options)
+      path = options[:path]
+      if path
+        path = File.expand_path(path)
+        netrc = Netrc.read(path)
+      else
+        netrc = Netrc.read
+      end
+
+      username, password = netrc['pinboard.in']
+      if username.nil? || password.nil?
+        puts "Couldn't read credentials from #{ path }".red
+        puts "Correct netrc syntax looks like:
+
+            machine pinboard.in
+              login email@foo.com
+              password mysekret
+          ".yellow
+        exit_now!(nil)
+      end
+
+      p netrc
       puts 'netrcc'
     end
 
