b5522fd5d659bb4f26898cc5c405405d4c8e17b8
Keith Smiley <keithbsmiley@gmail.com>
2013-04-25 12:06:10 -0400

    Default netrc location

--- lib/pinup/version.rb
+++ lib/pinup/version.rb
@@ -1,7 +1,8 @@
 module Pinup
-  VERSION      = '0.0.1'
-  SETTINGS     = File.expand_path('~/.pinup')
-  PINBOARD_URL = 'pinboard.in'
-  API_URL      = 'https://api.pinboard.in/v1'
-  JSON_PARAMS  = { format: 'json' }
+  VERSION       = '0.0.1'
+  SETTINGS      = File.expand_path('~/.pinup')
+  PINBOARD_URL  = 'pinboard.in'
+  API_URL       = 'https://api.pinboard.in/v1'
+  JSON_PARAMS   = { format: 'json' }
+  DEFAULT_NETRC = File.expand_path('~/.netrc')
 end
