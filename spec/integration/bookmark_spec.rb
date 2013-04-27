cc3518be3c3da80d554ed3b8f12787150cab8327
Keith Smiley <keithbsmiley@gmail.com>
2013-04-27 15:42:38 -0400

    Test class attributes

--- spec/integration/bookmark_spec.rb
+++ spec/integration/bookmark_spec.rb
@@ -1,6 +1,20 @@
 require_relative '../spec_helper'
 
 describe Bookmark do
+  describe 'attributes' do
+    before do
+      options = { 'href' => 'http://github.com', 'toread' => 'yes', 'tags' => '' }
+      @bookmark = Bookmark.new(options)
+    end
+
+    it 'should respond to the correct attributes' do
+      expect(@bookmark).to respond_to(:href)
+      expect(@bookmark).to respond_to(:unread)
+      expect(@bookmark).to respond_to(:untagged)
+      expect(@bookmark).not_to respond_to(:foobar)
+    end
+  end
+
   describe 'initialize' do
     describe 'unread without tags' do
       before do
