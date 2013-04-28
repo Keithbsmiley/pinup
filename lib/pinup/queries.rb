a830ef2ec013020cd9b33f201b7b67cb3700782b
Keith Smiley <keithbsmiley@gmail.com>
2013-04-28 18:14:16 -0400

    Delete items query

--- lib/pinup/queries.rb
+++ lib/pinup/queries.rb
@@ -58,6 +58,22 @@ module Pinup
       return new_items
     end
 
+    def self.delete_items(urls)
+      token = Pinup::Settings.get_token
+      if token.nil?
+        return nil
+      end
+
+      parameters = JSON_PARAMS.dup
+      parameters[:auth_token] = token
+
+      urls.each do |url|
+        url_params = parameters.dup
+        url_params[:url] = url
+        delete_query(url_params)
+      end
+    end
+
     def self.item_string(items)
       item_output = ""
       items.each do |item|
@@ -72,6 +88,18 @@ module Pinup
       def self.list_query(parameters)
         uri = URI.parse("#{ API_URL }/posts/all")
         uri.query = URI.encode_www_form(parameters)
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        http.request(request)
+      end
+
+      def self.delete_query(parameters)
+        uri = URI.parse("#{ API_URL }/posts/delete")
+        uri.query = URI.encode_www_forms(parameters)
 
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
