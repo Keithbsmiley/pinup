5966bd27dcff5cfdc9434a2b920ff37488ce54b0
Keith Smiley <keithbsmiley@gmail.com>
2013-04-27 20:07:42 -0400

    Filters and print them

--- lib/pinup/commands/list.rb
+++ lib/pinup/commands/list.rb
@@ -5,7 +5,8 @@ module Pinup
       untagged = options[:untagged]    unless options[:untagged].nil?
       count    = options[:number].to_i unless options[:number].nil?
       items    = Pinup::Queries.list_items(count)
-      Pinup::Queries.print_items(items, unread, untagged)
+      filtered = Pinup::Queries.filter_items(items, unread, untagged)
+      puts Pinup::Queries.item_string(filtered)
     end
   end
 end
