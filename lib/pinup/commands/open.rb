97f789b56ec1934f4c7286715dd8ef139d7faf4d
Keith Smiley <keithbsmiley@gmail.com>
2013-08-07 12:15:25 -0400

    Removed logging and escaping of URLs

--- lib/pinup/commands/open.rb
+++ lib/pinup/commands/open.rb
@@ -13,15 +13,9 @@ module Pinup
       items_string = Pinup::Queries.item_string(filtered)
       urls         = items_string.split(/\n/)
 
-      puts " got #{ count } urls"
-
       if count > 0 # For testing
         urls.delete_if do |url|
-          puts "opening #{ url }"
-          puts "cleared #{ URI.escape(url) }"
-          # %x[open "#{url}"]
-          Launchy.open(URI.escape(url)) do |exception|
-            puts "exception #{ exception }"
+          Launchy.open(url) do |exception|
             true # if there is an exception remove it from the array so it is not deleted
           end
           sleep(0.1)
